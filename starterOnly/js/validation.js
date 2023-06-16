d6cb6bcb9387090f3d4ab381ef06817c48a04f30
Flavie Bourcet <flavie.bourcet@gmail.com>
2023-06-16 15:47:53 +0200

    fix issue #3 -> add an error or validation message

--- starterOnly/js/validation.js
+++ starterOnly/js/validation.js
@@ -3,31 +3,46 @@ function validateForm(e) {
     const firstName = document.querySelector('#firstName').value;
     const lastName = document.querySelector('#lastName').value;
     const email = document.querySelector('#email').value;
+    const birthdate = document.querySelector('#birthdate').value;
     const tournamentQuantity = document.querySelector('#quantity').value;
     
     e.preventDefault()
-    if(validateFirstName(firstName) && validateLastName(lastName) && validateEmail(email) && validateTournamentQuantity(tournamentQuantity) && validateLocation() && validateTermsOfUse()) {
+    if(validateFirstName(firstName) && validateLastName(lastName) && validateEmail(email) && validateBirthdate(birthdate) && validateTournamentQuantity(tournamentQuantity) && validateLocation() && validateTermsOfUse()) {
         console.log("formulaire valide")
     }else {
         throw new Error;
     }
 };
 
+const formData = document.querySelectorAll('.formData')
+
 function validateFirstName(firstName){
 
     if (firstName === "" || firstName.length < 2) {
-        console.log('First name invalid');
+        formData[0].className = "formData";
+        formData[0].dataset.error = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+        formData[0].dataset.errorVisible = "true";
+        
         return false
     }else {
+        delete formData[0].dataset.error;
+        delete formData[0].dataset.errorVisible;
+        
         return true
     }
 }
 
 function validateLastName(lastName) {
     if (lastName === "" || lastName.length < 2) {
-        console.log('Last name invalid');
+        formData[1].className = "formData";
+        formData[1].dataset.error = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+        formData[1].dataset.errorVisible = "true";
+        
         return false
-    } else {
+    }else {
+        delete formData[1].dataset.error;
+        delete formData[1].dataset.errorVisible;
+        
         return true
     }
 }
@@ -35,18 +50,46 @@ function validateLastName(lastName) {
 function validateEmail(email) {
     const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     if (!email.match(regexEmail)) {
-        console.log('email invalid');
+        formData[2].className = "formData";
+        formData[2].dataset.error = "Veuillez entrer une adress email valide.";
+        formData[2].dataset.errorVisible = "true";
+        
         return false
-    } else {
+    }else {
+        delete formData[2].dataset.error;
+        delete formData[2].dataset.errorVisible;
+        
         return true
     }
 }
 
+function validateBirthdate(date){
+    if(!date) {
+        formData[3].className = "formData";
+        formData[3].dataset.error = "Vous devez entrer votre date de naissance.";
+        formData[3].dataset.errorVisible = "true";
+        
+        return false
+    }else {
+        delete formData[3].dataset.error;
+        delete formData[3].dataset.errorVisible;
+        
+        return true
+    }
+
+}
+
 function validateTournamentQuantity(quantity){
     if (quantity === "" || quantity < 0 || quantity > 99) {
-        console.log('tournament qt invalid');
+        formData[4].className = "formData";
+        formData[4].dataset.error = "Vous devez entrer une valeur numérique.";
+        formData[4].dataset.errorVisible = "true";
+
         return false
-    } else {
+    }else {
+        delete formData[4].dataset.error;
+        delete formData[4].dataset.errorVisible;
+
         return true
     }
 }
@@ -56,10 +99,16 @@ function validateLocation(){
 
     for (let i = 0; i < radios.length; i++) {
         if(radios[i].checked) {
+            delete formData[5].dataset.error;
+            delete formData[5].dataset.errorVisible;
+            
             return true
         }
     }
-    console.log('none selected');
+    formData[5].className = "formData";
+    formData[5].dataset.error = "Vous devez choisir une option.";
+    formData[5].dataset.errorVisible = "true";
+
     return false
 }
 
@@ -67,9 +116,15 @@ function validateTermsOfUse() {
     const checkbox = document.querySelector('#termsOfUse')
 
     if(checkbox.checked){
+        delete formData[6].dataset.error;
+        delete formData[6].dataset.errorVisible;
+        
         return true
     }else {
-        console.log('terms of use not checked');
+        formData[6].className = "formData";
+        formData[6].dataset.error = "Vous devez vérifier que vous acceptez les termes et conditions.";
+        formData[6].dataset.errorVisible = "true";
+
         return false
     }
 }
